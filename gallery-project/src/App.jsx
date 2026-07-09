5ac6fd503de562814f32b054ff80a1aa0ffa218e
shivolik <thakuraaarav11@gmail.com>
2026-07-09 21:45:56 +0530

    added button and page number and api limit changed to 10 from 30 and added loading message when data is being fetched

--- gallery-project/src/App.jsx
+++ gallery-project/src/App.jsx
@@ -1,32 +1,58 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { useEffect } from 'react'
 
 const App = () => {
   const [userdata, setUserdata] = useState([])
+  const [index, setindex] = useState(1)
   const getData = async () => {
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=30')
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
     setUserdata(response.data)
     console.log(response.data)
   }
-  let printUserData = 'No user available'
+useEffect (function() {
+  getData()
+}, [index])
+
+  let printUserData = 'Loading...'
 
   if (userdata.length > 0) {
     printUserData = userdata.map(function(elem,idx){
       return (
-      <div key={idx}>
+      <a href={elem.download_url} target='_blank' rel='noreferrer'>
+        <div key={idx}>
         <div className= 'h-40 w-40 bg-white ovwrflow-hidden rounded-lg'>
           <img className='h-full w-full object-cover ' src={elem.download_url} alt='' />
         </div>
         <h1 className='text-center'>{elem.author}</h1>
-      </div>)
+      </div>
+      </a>)
     })
   }
 
   return (
     <div className='h-screen  bg-black text-white overflow-auto'>
-      <button onClick={getData} className='bg-blue-500 p-2 rounded-lg m-4'>Get Data</button>
+      
       <div className='flex flex-wrap gap-4 p-4'>
         {printUserData}
+      </div>
+      <h1 className='felx text-2xl'>Page {index}</h1>
+      <div className='flex justify-center items-center p-4'>
+        <button className='bg-white text-black px-4 py-2 rounded-lg cursor-pointer active:scale-95'
+          onClick={()=>{
+            if(index > 1){
+              setindex(index - 1)
+            }
+          setUserdata([])
+              
+          }}>Prev
+        </button>
+        <button className='bg-white text-black px-4 py-2 rounded-lg mx-4 cursor-pointer active:scale-95'
+            onClick={()=>{
+              setindex(index + 1)
+              setUserdata([])
+            }}>Next
+        </button>
       </div>
     </div>
   )
